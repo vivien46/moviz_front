0a5220c6ac9cd7bf933062e3c37e59d42fcc613d
Vivien <vlad-vivien@orange.fr>
2023-04-18 14:17:25 +0200

    first commit

---
+++ components/Home.js
@@ -0,0 +1,82 @@
+import React, { useEffect, useState } from 'react';
+import styles from '../styles/Home.module.css';
+import 'antd/dist/antd.css';
+import { Popover, Button } from 'antd';
+import Movie from '../components/Movie';
+
+
+export default function Home() {
+
+const [lastMovies, setLastMovies] = useState([]);
+const [ moviesList, setMoviesList] = useState([]);
+
+useEffect ( () => {
+  fetch('http://localhost:4000/movies')
+  .then((res) => res.json())
+  .then((data) => {
+    setMoviesList(data.movies);
+  })
+  console.log(moviesList)
+}, [])
+
+const updateLastMovies = (movieTitle) => {
+  const found = lastMovies.find(el => el === movieTitle)
+  if (lastMovies){
+
+  }
+  setLastMovies ([...lastMovies, movieTitle]);
+}
+
+// const moviesData = [
+//     { title: 'Forrest Gump', poster: 'forrestgump.jpg', voteAverage: 9.2, voteCount: 22_705, overview: 'A man with a low IQ has accomplished great things in his life and been present during significant historic events—in each case.' },
+//     { title: 'The Dark Knight', poster: 'thedarkknight.jpg', voteAverage: 8.5, voteCount: 27_547, overview: 'Batman raises the stakes in his war on crime and sets out to dismantle the remaining criminal organizations that plague the streets.' },
+//     { title: 'Your name', poster: 'yourname.jpg', voteAverage: 8.5, voteCount: 8_691, overview: 'High schoolers Mitsuha and Taki are complete strangers living separate lives. But one night, they suddenly switch places.' },
+//     { title: 'Iron Man', poster: 'ironman.jpg', voteAverage: 7.6, voteCount: 22_7726, overview: 'After being held captive in an Afghan cave, billionaire engineer Tony Stark creates a unique weaponized suit of armor to fight evil.' },
+//     { title: 'Inception', poster: 'inception.jpg', voteAverage: 8.4, voteCount: 31_546, overview: 'Cobb, a skilled thief who commits corporate espionage by infiltrating the subconscious of his targets is offered a chance to regain his old life.' },
+//   ];
+
+//faire un map sur le tableau d'objets moviesData
+//créér les props a faire passer dans le composant movie du map()
+// Faire passer les props vers le composant movie
+
+const films = moviesList.map((el, i) => {
+  return (
+    <Movie
+     title={el.title}
+     poster="https://image.tmdb.org/t/p/w500/${poster_path}"
+     voteAverage={el.voteAverage}
+     voteCount={el.voteCount}
+     overview={el.overview}
+     updateLastMovies={updateLastMovies}
+     key={i}
+     />
+  );
+  console.log(moviesList);
+});
+
+  
+    const popoverContent = lastMovies.map((el,i) => {
+    return (
+      <span key={i} className={styles.popoverContent}>{el}
+        <FontAwesomeIcon icon={faCircleXmark} onClick={() => updateLikedMovies(el)}/>
+        </span>
+    ); 
+   });
+  
+
+  return (
+  <div className={styles.main}>
+    <div className={styles.header}>
+      <div className={styles.logoContainer}>
+        <img src="logo.png" alt="logo" />
+        <p className={styles.title}>Movies</p>
+      </div>
+      <Popover title='Liked Movies' content={popoverContent} trigger="click">
+        <Button>❤️ 4 movie(s)</Button>
+      </Popover>
+    </div>
+  <div className={styles.release}>Last Release</div>
+  <div className={styles.moviesContainer}>{films}</div>
+  </div>
+  );
+  }
---
+++ components/Movie.js
@@ -0,0 +1,88 @@
+import React, { useState } from "react";
+import styles from '../styles/Movie.module.css';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { faStar, faVideo, faHeart } from '@fortawesome/free-solid-svg-icons';
+
+function Movie(props) {
+    const stars =[];
+
+    for ( let i = 0; i< 10; i++){
+        let style = {};
+        if ( i < props.voteAverage - 1){
+        style = {color : '#f1c40f'};
+            
+        }
+        stars.push(<FontAwesomeIcon icon={faStar} key={i} style={style}/>);
+        
+    }
+    
+    
+    // Définisez les trois états suivants avec leur setter
+    //personalNote initialisé à 0
+    // WatchCount initialisé à 0
+    // Like initialisé à false
+
+    const [personalNote, setpersonalNote] = useState(0);
+    const [watchCount, setwatchCount] = useState(0);
+    const [like, setlike] = useState(false);
+
+    //2
+    // Créer le système des étoiles de notation personnelle #2196f3
+    const persostars = [];
+    for ( let i = 0; i< 10; i++){
+        let starstyle = { cursor : 'pointer'};
+        if ( i < personalNote){
+        starstyle = {color : '#2196f3', cursor: 'pointer'};
+            
+        }
+        persostars.push(<FontAwesomeIcon icon={faStar} onClick={() => setpersonalNote(i + 1)} style={starstyle} key={i} />);
+    }
+
+    // Au clic sur l'étoile, vous attriburez la note sous forme d'étoile
+    // entre parenthèses affichez la note d'après les étoiles
+
+    //3
+   // au clic sur la camera l'icone deviendra rouge #e74c3c et on incrémente à chaque clic
+    const handleMovie = () =>{
+        setwatchCount(watchCount + 1);
+    };
+    let videoIconStyle ={
+        cursor: 'pointer'
+    }
+    if (watchCount){
+        videoIconStyle = {color: '#e74c3c', cursor: 'pointer'};
+    }
+
+   //4 ==> le bouton Like
+   //Mettre en place le systeme de like : au clic le coeur devient rouge et au re-clic il redeviendra noir
+    const handleLike =() =>{
+        setlike(!like);
+        props.updateLastMovies(props.title);
+    };
+
+    let heartIconStyle = {cursor : 'pointer'};
+    if(like){
+        heartIconStyle = { color: '#e74c3c', cursor: 'pointer'};
+    };
+
+    return (
+    <div className={styles.card}>
+        <img className={styles.image} src={props.poster} alt={props.title} />
+        <div className={styles.textContainer}>
+            <span className={styles.name}>{props.title}</span>
+            <p className={styles.description}>{props.overview}</p>
+        </div>
+        <span className={styles.vote}>{stars} {props.voteCount}</span>
+        <span className={styles.vote}>{persostars} ({personalNote})</span>
+        <span>
+        <FontAwesomeIcon icon={faVideo} style={videoIconStyle} onClick={() => handleMovie()} />
+        ({watchCount})
+        </span>
+        <span>
+        <FontAwesomeIcon icon={faHeart} style={heartIconStyle} onClick={() => handleLike()} />
+        </span>
+    </div>
+    );
+  }
+
+export default Movie;
